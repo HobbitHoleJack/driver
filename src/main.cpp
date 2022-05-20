c39761f1ebb9bd87d78e55df32720fede3c0d0b7
JackReiman <Jreiman086@gmail.com>
2022-05-20 15:36:12 -0700

    heading measure

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,21 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
-
-
+	#define LR_distance_from_center 5
+	#define pi 3.14159
+	double x_pos = 0;
+	double y_pos = 0;
+	int heading_rad = 0;
+	int heading;
 
 	pros::ADIEncoder left_enc ('A', 'B', false);
 	pros::ADIEncoder right_enc ('C', 'D', false);
 	pros::ADIEncoder back_enc ('E', 'F', false);
 	// 1 tick on encoders should == 0.0283616"
+
+	heading_rad = (left_enc.get_value() - right_enc.get_value()) / LR_distance_from_center;
+	heading = heading_rad * (180/pi);
+	
 } 
 
 void opcontrol() {}
