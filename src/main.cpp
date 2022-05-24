fdc3d262f74b9c82d05074a99b356d1f437be880
Jack Reiman <Jreiman086@gmail.com>
2022-05-24 09:27:54 -0700

    odometry task

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include "main.h"
+#include "pros/rtos.hpp"
+
+int heading;
+
+double x_pos = 0;
+double y_pos = 0;
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -24,21 +30,39 @@ void disabled() {}
  */
 void competition_initialize() {}
 
-void autonomous() {
+
+void odometry_fn() {
+
 	#define LR_distance_from_center 5
 	#define pi 3.14159
-	double x_pos = 0;
-	double y_pos = 0;
-	int heading_rad = 0;
-	int heading;
+
+	int heading_rad{0};
+	double current_l{0};
+	double current_r{0};
+	double current_b{0};
 
 	pros::ADIEncoder left_enc ('A', 'B', false);
 	pros::ADIEncoder right_enc ('C', 'D', false);
 	pros::ADIEncoder back_enc ('E', 'F', false);
 	// 1 tick on encoders should == 0.0283616"
 
-	heading_rad = (left_enc.get_value() - right_enc.get_value()) / LR_distance_from_center;
-	heading = heading_rad * (180/pi);
+	while (1) {
+
+		current_l = left_enc.get_value();
+		current_r = right_enc.get_value();
+		current_b = back_enc.get_value();
+
+		heading_rad = (current_l - current_r) / LR_distance_from_center;
+		heading = heading_rad * (180/pi);
+
+		pros::delay(10);
+	}
+	
+}
+
+void autonomous() {
+	
+	pros::Task odometry(odometry_fn);
 	
 } 
 
