9fac99ae66a92d760a57d749715e46385cf95507
JackReiman <Jreiman086@gmail.com>
2022-06-28 09:01:21 -0700

    i don't want okapi right now

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,7 @@
 #include "pros/rtos.hpp"
 #include "autoSelect/selection.h"
 #include <string>
-using namespace okapi;
 int auton_side = -1;
-
-std::shared_ptr<OdomChassisController> chassis =
-		ChassisControllerBuilder()
-		.withMotors(1, -2) // left motor is 1, right motor is 2 (reversed)
-		.withGains(
-			{0.001, 0, 0.0001}, // distance controller gains
-			{0.001, 0, 0.0001}, // turn controller gains
-			{0.001, 0, 0.0001}  // angle controller gains (helps drive straight)
-		)
-		.withSensors(
-			ADIEncoder{'A', 'B'}, // left encoder in ADI ports A & B
-			ADIEncoder{'C', 'D', true},  // right encoder in ADI ports C & D (reversed)
-			ADIEncoder{'E', 'F'}  // middle encoder in ADI ports E & F
-		)
-		// green gearset, tracking wheel diameter (2.75 in), track (7 in), and TPR (360)
-		// 1 inch middle encoder distance, and 2.75 inch middle wheel diameter
-		.withDimensions(AbstractMotor::gearset::blue, {{3.25_in, 7_in, 1_in, 3.25_in}, quadEncoderTPR})
-		.withOdometry() // use the same scales as the chassis (above)
-		.buildOdometry(); // build an odometry chassis
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
